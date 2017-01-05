c4ac1ad18e81804d19ff7db82e703eb35b981dbd
SHIKHAR DEEP <deepshikhar24@yahoo.com>
2017-01-06 00:10:55 +0530

    implemented cursor adapter and listview for displaying petinfo

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.android.pets;
 
 import android.content.ContentValues;
@@ -26,9 +11,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 import com.example.android.pets.data.PetContract.PetEntry;
+import com.example.android.pets.data.PetCursorAdapter;
 import com.example.android.pets.data.PetDbHelper;
+
+import java.util.List;
 
 /**
  * Displays list of pets that were entered and stored in the app.
@@ -106,6 +95,11 @@ public class CatalogActivity extends AppCompatActivity {
                 null,
                 null);
 
+        ListView petListView = (ListView) findViewById(R.id.list);
+        PetCursorAdapter adapter = new PetCursorAdapter(this,cursor);
+        petListView.setAdapter(adapter);
+
+        /* ################ COMMENTED 'CAUSE OF LISTVIEW ##################
 
         TextView displayView = (TextView) findViewById(R.id.text_view_pet);
 
@@ -152,7 +146,7 @@ public class CatalogActivity extends AppCompatActivity {
             // resources and makes it invalid.
             cursor.close();
         }
-    }
+*/    }
 
 
 
---
+++ app/src/main/java/com/example/android/pets/data/PetCursorAdapter.java
@@ -0,0 +1,66 @@
+package com.example.android.pets.data;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+/import com.example.android.pets.R;
+
+import org.w3c.dom.Text;
+
+import static android.R.attr.name;
+
+public class PetCursorAdapter extends CursorAdapter {
+
+
+    public PetCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0 /* flags */);
+    }
+
+    /**
+     * Makes a new blank list item view. No data is set (or bound) to the views yet.
+     *
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already
+     *                moved to the correct position.
+     * @param parent  The parent to which the new view is attached to
+     * @return the newly created list item view.
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        // Fill out this method and return the list item view (instead of null)
+            return LayoutInflater.from(context).inflate(R.layout.list_item,parent,false);
+        return null;
+    }
+
+    /**
+     * This method binds the pet data (in the current row pointed to by cursor) to the given
+     * list item layout. For example, the name for the current pet can be set on the name TextView
+     * in the list item layout.
+     *
+     * @param view    Existing view, returned earlier by newView() method
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already moved to the
+     *                correct row.
+     */
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+        TextView nameTextView = (TextView)view.findViewById(R.id.name);
+        TextView summarytextView = (TextView)view.findViewById(R.id.summary);
+
+        //for column index see word doc page no 10
+        int nameColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_NAME);
+        int breedColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_BREED);
+
+        String petName = cursor.getString(nameColumnIndex);
+        String petBreed = cursor.getString(breedColumnIndex);
+
+        nameTextView.setText(petName);
+        summarytextView.setText(petBreed);
+    }
+}
